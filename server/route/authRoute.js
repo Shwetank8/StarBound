38936832fa5290c3da00e2351eb702ecd2478651
Shwetank Sinha <137472512+Shwetank8@users.noreply.github.com>
2024-09-14 17:14:45 +0530

    Update authRoute.js

--- server/route/authRoute.js
+++ server/route/authRoute.js
@@ -4,7 +4,7 @@ const { signup, login } = require('../controller/authController');
 
 const router = express.Router();
 
-router.post('signup', signup);
-router.post('login', login);
+router.post('/signup', signup);
+router.post('/login', login);
 
 module.exports = router;
